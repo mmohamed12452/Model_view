b3d37bce3323aa9aef23bc2bdcd7c44333626fb6
moassamo <moassermo@gmail.com>
2023-03-22 13:22:09 -0600

    set up server

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 const express = require("express");
-const sequelize = require("")
+const sequelize = require("./config/connection");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const { urlencoded } = require("express");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret :'supersecretsessionsecrettext',
+    cookie:{ maxAge: 180000},
+    resave:false,
+    saveUninitialized:true,
+    store:new SequelizeStore({
+        db:sequelize
+    })
+
+};
+
+const hbs = exphbs.create({helpers});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session(sess));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
+  });

20e6b754891d4fa16c85b1f066465df7ab183c80
moassamo <moassermo@gmail.com>
2023-03-23 14:18:11 -0600

    deployment connection unsucessful

--- config/connection.js
+++ config/connection.js
@@ -10,10 +10,10 @@ if(process.env.JAWSDB_URL)
 else
 {
     sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-        host:'localhost',
+        // host:'localhost',
         dialect:'mysql',
-        port:3306,
-        password:'Password'       
+        // port:3306,
+        // password:'Password'       
 
     });
 }
